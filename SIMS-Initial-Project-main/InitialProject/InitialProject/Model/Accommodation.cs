fdbe80cdfc9bb380d2c48c91e36800cd64841339
anatadic <anatadic111@gmail.com>
2023-03-11 18:46:25 +0100

    Added accommodation overview

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Model/Accommodation.cs
@@ -0,0 +1,54 @@
+using InitialProject.Serializer;
+using System;
+using System.Linq;
+
+namespace InitialProject.Model
+{
+    public enum AccommodationType
+    {
+        apartment,
+        house, 
+        cottage
+    }
+    public class Accommodation : ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public AccommodationType Type { get; set; }
+        public int GuestsNumber { get; set; }
+        public int ReservationDays { get; set; }
+
+
+        public Accommodation() { }
+
+        public Accommodation(string name, string city, string country, AccommodationType type, int guestsNumber, int reservationDays)
+        {
+            Name = name;
+            City = city;
+            Country = country;
+            Type = type;
+            GuestsNumber = guestsNumber;
+            ReservationDays = reservationDays;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Name, City, Country, Type.ToString(), GuestsNumber.ToString(), ReservationDays.ToString()};
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Name = values[1];
+            City = values[2];
+            Country = values[3];
+            Type = (AccommodationType) Enum.Parse(typeof(AccommodationType), values[4], false);
+            GuestsNumber = Convert.ToInt32(values[5]);
+            ReservationDays = Convert.ToInt32(values[6]);
+
+        }
+    }
+}
