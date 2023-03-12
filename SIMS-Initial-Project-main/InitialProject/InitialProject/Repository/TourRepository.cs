0388aeff4c0f38b6791350d65aec6661548c66bb
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-03-12 15:54:35 +0100

    Add files via upload

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Repository/TourRepository.cs
@@ -0,0 +1,33 @@
+﻿using InitialProject.Model;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Repository
+{
+    public class TourRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tour.csv";
+
+        private readonly Serializer<Tour> _serializer;
+
+        private List<Tour> _tours;
+
+        public TourRepository()
+        {
+            _serializer = new Serializer<Tour>();
+            _tours = _serializer.FromCSV(FilePath);
+
+        }
+
+        public List<Tour> GetAllTours()
+        {
+            _tours = _serializer.FromCSV(FilePath);
+            return _tours;
+        }
+
+    }
+}
