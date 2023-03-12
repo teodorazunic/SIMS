75dfd7b5f32b5b892ea87f851a98de8f482fee60
teodorazunic <teodorazunic@gmail.com>
2023-03-12 11:32:46 +0100

    Tour model

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Model/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model
+{
+    public class Location
+    {
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Adress { get; set; }
+
+    }
+}
