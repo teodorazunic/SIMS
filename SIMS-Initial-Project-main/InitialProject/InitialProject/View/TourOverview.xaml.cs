7b6ee46c082697dd36e1376ca4a094e2b596722d
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-03-13 23:34:18 +0100

    Showing tours

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/View/TourOverview.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using InitialProject.Model;
+using InitialProject.Repository;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InitialProject
+{
+    /// <summary>
+    /// Interaction logic for TourOverview.xaml
+    /// </summary>
+    public partial class TourOverview : Window
+    {
+        public static ObservableCollection<Tour> Tours { get; set; }
+
+        public User LoggedInUser { get; set; }
+
+        private readonly TourRepository _repository;
+
+        public TourOverview(User user)
+        {
+
+            InitializeComponent();
+            DataContext = this;
+            LoggedInUser = user;
+            _repository = new TourRepository();
+            Tours = new ObservableCollection<Tour>(_repository.GetAllTours());
+        }
+    }
+}
