12c0b061582edf62050c9f63a68994f19e709b4e
medojevicnana <medojevicnana@gmail.com>
2023-03-13 20:46:36 +0100

    updated accommodation repository

--- SIMS-Initial-Project-main/InitialProject/InitialProject/Repository/AccommodationRepository.cs
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Repository/AccommodationRepository.cs
@@ -2,6 +2,7 @@ using InitialProject.Model;
 using InitialProject.Serializer;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace InitialProject.Repository
 {
@@ -24,5 +25,49 @@ namespace InitialProject.Repository
             _accommodations = _serializer.FromCSV(FilePath);
             return _accommodations;
         }
+
+        public Accommodation Save(Accommodation accommodation)
+        {
+            accommodation.Id = NextId();
+            _accommodations = _serializer.FromCSV(FilePath);
+            _accommodations.Add(accommodation);
+            _serializer.ToCSV(FilePath, _accommodations);
+            return accommodation;
+        }
+
+        public int NextId()
+        {
+            _accommodations = _serializer.FromCSV(FilePath);
+            if (_accommodations.Count < 1)
+            {
+                return 1;
+            }
+            return _accommodations.Max(a => a.Id) + 1;
+        }
+
+
+        public void Delete(Accommodation accommodation)
+        {
+            _accommodations = _serializer.FromCSV(FilePath);
+            Accommodation founded = _accommodations.Find(a => a.Id == accommodation.Id);
+            _accommodations.Remove(founded);
+            _serializer.ToCSV(FilePath, _accommodations);
+        }
+
+        public Accommodation Update(Accommodation accommodation)
+        {
+            _accommodations = _serializer.FromCSV(FilePath);
+            Accommodation current = _accommodations.Find(a => a.Id == accommodation.Id);
+            int index = _accommodations.IndexOf(current);
+            _accommodations.Remove(current);
+            _accommodations.Insert(index, accommodation);       // keep ascending order of ids in file 
+            _serializer.ToCSV(FilePath, _accommodations);
+            return accommodation;
+        }
+
+
+
+
     }
+
 }
