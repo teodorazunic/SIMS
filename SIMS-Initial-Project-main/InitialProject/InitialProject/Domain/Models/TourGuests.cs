68a210ccad18c91275f4b504bcb2c0aa43004715
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-04-13 15:58:01 +0200

    added new models

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Domain/Models/TourGuests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using InitialProject.Serializer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Domain.Models
+{
+    public class TourGuests : User
+    {
+
+        public int Age { get; set; }
+
+        public List<Tour> ReservedTours { get; set; }
+
+        public List<Voucher> AvailableVouchers { get; set; }
+        
+        public TourGuests() { }
+
+        public TourGuests(int age, List<Tour> reservedTours, List<Voucher> availableVouchers)
+        {
+            Age = age;
+            ReservedTours = reservedTours;
+            AvailableVouchers = availableVouchers;
+        }
+    }
+}
---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Domain/Models/Voucher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using InitialProject.Serializer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Domain.Models
+{
+    public class Voucher : ISerializable
+    {
+        public string Title { get; set; }
+
+        public DateOnly ValidUntil { get; set; }
+
+        public int GuideId { get; set; }
+
+        public int GuestId { get; set; }
+
+        public Voucher() { }
+
+        public Voucher(string title, DateOnly validUntil, int guideId, int guestId)
+        {
+            Title = title;
+            ValidUntil = validUntil;
+            GuideId = guideId;
+            GuestId = guestId;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] ToCSV()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
