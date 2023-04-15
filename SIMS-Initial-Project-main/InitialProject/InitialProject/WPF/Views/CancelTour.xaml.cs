17fe0e7fa2a704b1653ec5f469690cce1248de2a
teodorazunic <teodorazunic@gmail.com>
2023-04-15 16:29:49 +0200

    Final cancel tour

--- SIMS-Initial-Project-main/InitialProject/InitialProject/WPF/Views/CancelTour.xaml.cs
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/WPF/Views/CancelTour.xaml.cs
@@ -1,4 +1,6 @@
-﻿using InitialProject.Domain.Models;
+﻿using InitialProject.Domain.Model;
+using InitialProject.Domain.Models;
+using InitialProject.Forms;
 using InitialProject.Repository;
 using System;
 using System.Collections.Generic;
@@ -24,7 +26,9 @@ namespace InitialProject.WPF.Views
     /// </summary>
     public partial class CancelTour : Window
     {
-        public static ObservableCollection<Tour> Tours { get; set; }
+        //public static ObservableCollection<Tour> Tours { get; set; }
+
+        private const string FilePath = "../../../Resources/Data/tour.csv";
 
         public User LoggedInUser { get; set; }
 
@@ -56,7 +60,22 @@ namespace InitialProject.WPF.Views
             DataContext = this;
             LoggedInUser = user;
             _repository = new TourRepository();
-            Tours = new ObservableCollection<Tour>(_repository.GetAllTours());
+            //Tours = new ObservableCollection<Tour>(_repository.GetPendingTours(FilePath));
+            Tours.ItemsSource = _repository.GetPendingTours();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(_selectedTour != null)
+            {
+                string message = _repository.CancelTour(_selectedTour);
+
+                MessageBox.Show(message);
+                 
+                
+            }
+            Tours.Items.Refresh();
         }
     }
 }
