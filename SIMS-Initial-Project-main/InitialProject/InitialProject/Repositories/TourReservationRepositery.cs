b1d9de5f91dcd05d73a7f49b2fcf4d4013c1d8db
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-04-15 16:30:39 +0200

    new repos

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Repositories/TourReservationRepositery.cs
@@ -0,0 +1,67 @@
+﻿using InitialProject.Domain.Model;
+using InitialProject.Domain.Models;
+using InitialProject.Domain.RepositoryInterfaces;
+using InitialProject.Forms;
+using InitialProject.Repository;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Repositories
+{
+    public class TourReservationRepositery : ITourReservationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tourreservations.csv";
+
+        private readonly Serializer<TourReservations> _serializer;
+
+        private TourRepository _tourRepository;
+
+        private List<TourReservations> _tourreservations;
+
+        public List<TourReservations> GetAllReservationsByGuestId(int GuestId)
+        {
+            List<TourReservations> allReservations = _serializer.FromCSV(FilePath);
+
+            List<TourReservations> reservationsByGuest = allReservations
+                .Where(reservation => reservation.GuestId == GuestId)
+                .ToList();
+
+            return reservationsByGuest;
+        }
+
+        public List<TourReservations> GetAllReservationsByTourId(int TourId)
+        {
+            List<TourReservations> allReservations = _serializer.FromCSV(FilePath);
+
+            List<TourReservations> reservationsByTour = allReservations
+                .Where(reservation => reservation.TourId == TourId)
+                .ToList();
+
+            return reservationsByTour;
+        }
+
+        public TourReservations SaveReservation(TourReservations tourreservations)
+        {
+
+            tourreservations.TourReservationId = NextId();
+            _tourreservations = _serializer.FromCSV(FilePath);
+            _tourreservations.Add(tourreservations);
+            _serializer.ToCSV(FilePath, _tourreservations);
+            return tourreservations;
+        }
+
+        public int NextId()
+        {
+            _tourreservations = _serializer.FromCSV(FilePath);
+            if (_tourreservations.Count < 1)
+            {
+                return 1;
+            }
+            return _tourreservations.Max(t=> t.TourReservationId) + 1;
+        }
+    }
+}
---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Repositories/VoucherRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using InitialProject.Domain.Model;
+using InitialProject.Domain.RepositoryInterfaces;
+using InitialProject.Serializer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialProject.Domain.Models;
+
+namespace InitialProject.Repositories
+{
+    public class VoucherRepository : IVoucherRepository
+    {
+        private const string FilePath = "../../../Resources/Data/vouchers.csv";
+
+        private readonly Serializer<Voucher> _serializer;
+
+        private List<Voucher> _vouchers;
+
+        public VoucherRepository()
+        {
+            _serializer = new Serializer<Voucher>();
+            _vouchers = _serializer.FromCSV(FilePath);
+
+        }
+
+        public void DeleteVoucher()
+        {
+            _vouchers = _serializer.FromCSV(FilePath);
+            List<Voucher> Vouchers = _vouchers;
+            foreach (Voucher voucher in Vouchers)
+            {
+                if (voucher.ValidUntil == DateTime.Now)
+                {
+                    ;
+                }
+            }
+        }
+
+        public Voucher GetVoucherByGuestId(int guestId)
+        {
+            _vouchers = _serializer.FromCSV(FilePath);
+            return _vouchers.Find(v => v.GuestId == guestId);
+        }
+
+        public Voucher GetVoucherByGuideId(int guideId)
+        {
+            _vouchers = _serializer.FromCSV(FilePath);
+            return _vouchers.Find(v => v.GuideId == guideId);
+        }
+
+        public Voucher GetVoucherById(int voucherId)
+        {
+            _vouchers = _serializer.FromCSV(FilePath);
+            return _vouchers.Find(v => v.VoucherId == voucherId);
+        }
+
+        public int NextId()
+        {
+            _vouchers = _serializer.FromCSV(FilePath);
+            if (_vouchers.Count < 1)
+            {
+                return 1;
+            }
+            return _vouchers.Max(v => v.VoucherId) + 1;
+        }
+    }
+}
