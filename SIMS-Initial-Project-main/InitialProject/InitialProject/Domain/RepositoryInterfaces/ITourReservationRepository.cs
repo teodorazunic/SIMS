1b20f5130a7d83a50411bf73f00873539277864e
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-04-15 16:25:25 +0200

    added interfaces for repos

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Domain/RepositoryInterfaces/ITourReservationRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using InitialProject.Domain.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialProject.Domain.Models;
+
+namespace InitialProject.Domain.RepositoryInterfaces
+{
+    public interface ITourReservationRepository
+    {
+        public List<TourReservations> GetAllReservationsByTourId(int TourId);
+
+        public List<TourReservations> GetAllReservationsByGuestId(int GuestId);
+
+        public void SaveReservation(TourReservations tourreservations);
+
+        public int NextId();
+
+
+    }
+}
