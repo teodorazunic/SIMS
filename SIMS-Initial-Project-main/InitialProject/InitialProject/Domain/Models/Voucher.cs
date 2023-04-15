f331e23dd7bb8d40c8eca53bba34799621658cae
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-04-15 16:27:38 +0200

    small fixes

--- SIMS-Initial-Project-main/InitialProject/InitialProject/Domain/Models/Voucher.cs
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Domain/Models/Voucher.cs
@@ -9,9 +9,11 @@ namespace InitialProject.Domain.Models
 {
     public class Voucher : ISerializable
     {
+        public int VoucherId { get; set; }
+        
         public string Title { get; set; }
 
-        public DateOnly ValidUntil { get; set; }
+        public DateTime ValidUntil { get; set; }
 
         public int GuideId { get; set; }
 
@@ -19,8 +21,9 @@ namespace InitialProject.Domain.Models
 
         public Voucher() { }
 
-        public Voucher(string title, DateOnly validUntil, int guideId, int guestId)
+        public Voucher(int voucherId, string title, DateTime validUntil, int guideId, int guestId)
         {
+            VoucherId = voucherId;
             Title = title;
             ValidUntil = validUntil;
             GuideId = guideId;
@@ -29,12 +32,17 @@ namespace InitialProject.Domain.Models
 
         public void FromCSV(string[] values)
         {
-            throw new NotImplementedException();
+            VoucherId = Convert.ToInt32(values[0]);
+            Title = values[1];
+            ValidUntil = Convert.ToDateTime(values[2]);
+            GuideId = Convert.ToInt32(values[3]);
+            GuestId = Convert.ToInt32(values[4]);
         }
 
         public string[] ToCSV()
         {
-            throw new NotImplementedException();
+            string[] csvValues = { VoucherId.ToString(), Title, ValidUntil.ToString("dd-MMM-y HH:mm:ss tt"),GuideId.ToString(), GuestId.ToString() };
+            return csvValues;
         }
     }
 }
