f04d8e6eb45f897f7feac589dedd1f87616e9caa
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-05-10 22:20:28 +0200

    Update TourReservationRepositery.cs

--- SIMS-Initial-Project-main/InitialProject/InitialProject/Repositories/TourReservationRepositery.cs
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Repositories/TourReservationRepositery.cs
@@ -5,10 +5,8 @@ using InitialProject.Forms;
 using InitialProject.Repository;
 using InitialProject.Serializer;
 using InitialProject.WPF.Views.Guest2;
-using InitialProject.View;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,89 +21,24 @@ namespace InitialProject.Repositories
 
         private TourRepository _tourRepository;
 
-        
-
         private List<TourReservations> _tourreservations;
         
         public TourReservationRepositery()
         {
             _serializer = new Serializer<TourReservations>();
-            _tourreservations = _serializer.FromCSV(FilePath);
             _tourRepository = new TourRepository();
-            
-        }
-
-        public TourReservations FindMostAttendantTourByYear(string filename, string year)
-        {
-            TourReservations tourReservation = new TourReservations();
-            List<TourReservations> tourReservations = ReadFromTourReservationsCsv(filename);
-            List<Tour> tours = _tourRepository.GetAllTours();
-            int maxGuests = 0;
-            for (int i = 0; i < tours.Count; i++)
-            {
-                for (int j = 0; j < tourReservations.Count; j++)
-                {
-                    if (tours[i].Id == tourReservations[j].TourId && tourReservations[j].NumberOfGuests > maxGuests && tours[i].Start.Year == Convert.ToInt32(year))
-                    {
-                        maxGuests = tourReservations[j].NumberOfGuests;
-                        tourReservation = tourReservations[j];
-
-                    }
-                }
-            }
-
-            return tourReservation;
-
-        }
-
-        public TourReservations FindMostAttendantTour(string filename)
-        {
-            TourReservations tourReservation = new TourReservations();
-            List<TourReservations> tourReservations = ReadFromTourReservationsCsv(filename);
-            int maxGuests = 0;
-
-            for(int i =0; i < tourReservations.Count; i++)
-            {
-                if (tourReservations[i].NumberOfGuests > maxGuests)
-                {
-                    maxGuests = tourReservations[i].NumberOfGuests;
-                    tourReservation = tourReservations[i];
-
-                }
-            }
-
-            return tourReservation;
-        }
-
-        public List<TourReservations> ReadFromTourReservationsCsv(string filename)
-        {
-            List<TourReservations> tourReservations = new List<TourReservations>();
-            
-            using (StreamReader sr = new StreamReader(filename))
-            {
-                while (!sr.EndOfStream)
-                {
-                    string line = sr.ReadLine();
-
-                    string[] fields = line.Split('|');
-                    TourReservations tourReservation = new TourReservations();
-                    tourReservation.TourReservationId = Convert.ToInt32(fields[0]);
-                    tourReservation.TourId = Convert.ToInt32(fields[1]);
-                    tourReservation.GuestId = Convert.ToInt32(fields[2]);
-                    tourReservation.NumberOfGuests = Convert.ToInt32(fields[3]);
-                    tourReservation.UsedVoucher = Convert.ToBoolean(fields[4]);
-                    tourReservations.Add(tourReservation);
-
-
-                }
-            }
-            return tourReservations;
+            _tourreservations = _serializer.FromCSV(FilePath);
         }
 
         public List<TourReservations> GetAllReservationsByGuestId(int GuestId)
         {
-            _tourreservations = _serializer.FromCSV(FilePath);
-            return _tourreservations.FindAll(t => t.GuestId == GuestId);
+            List<TourReservations> allReservations = _serializer.FromCSV(FilePath);
+
+            List<TourReservations> reservationsByGuest = allReservations
+                .Where(reservation => reservation.GuestId == GuestId)
+                .ToList();
+
+            return reservationsByGuest;
 
         }
 
@@ -118,6 +51,12 @@ namespace InitialProject.Repositories
                 .ToList();
 
             return reservationsByTour;
+        }
+
+        public TourReservations GetTourReservationByTourId(int TourId)
+        {
+            _tourreservations = _serializer.FromCSV(FilePath);
+            return _tourreservations.Find(t => t.TourId == TourId);
         }
 
         public List<TourReservations> CheckReservedTourStatus()
@@ -133,6 +72,35 @@ namespace InitialProject.Repositories
                 }
             }
             return endedTours;
+        }
+
+        public List<Tour> GetEndedToursByGuestId(int GuestId)
+        {
+            List<Tour> selectedTour = _tourRepository.GetAllTours();
+            List<TourReservations> endedTour = CheckReservedTourStatus();
+            for(int i = 0; i < endedTour.Count(); i++)
+            {
+                if (endedTour[i].GuestId == GuestId)
+                {
+                    Tour tour = _tourRepository.GetTourById(endedTour[i].TourId);
+                    selectedTour.Add(tour);
+                }
+            }
+            return selectedTour;
+        }
+
+        public List<TourReservations> GetNotRatedTours(int GuestId) 
+        {
+            List<TourReservations> notRated = new List<TourReservations>();
+            notRated = GetAllReservationsByGuestId(GuestId);
+            for(int i = 0; i < notRated.Count(); i++)
+            {
+                if (notRated[i].Status == "Not")
+                {
+                    notRated.Add(notRated[i]);
+                }
+            }
+            return notRated;
         }
 
         public void SaveReservation(TourReservations tourreservations)
