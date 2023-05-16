1d84e6ee53e1a3ab2662961fab8f5254852440ae
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-05-16 17:06:19 +0200

    Add files via upload

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/WPF/Views/Guest2/TourRequestOverview.xaml.cs
@@ -0,0 +1,163 @@
+﻿using InitialProject.WPF.Views.Guest2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using InitialProject.Domain.Models;
+using InitialProject.Forms;
+using InitialProject.Repository;
+using InitialProject.View;
+using InitialProject.WPF.Views;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using InitialProject.Repositories;
+
+namespace InitialProject.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for TourRequestOverview.xaml
+    /// </summary>
+    public partial class TourRequestOverview : Window
+    {
+        public User LoggedInUser { get; set; }
+
+        public static ObservableCollection<TourRequest> TourRequests { get; set; }
+        private readonly TourRequestRepository _repository;
+
+
+        private string _city = "";
+        private string _status = "";
+        private string _type = "";
+        private DateTime _startDate = DateTime.MinValue;
+
+        public string City
+        {
+            get => _city;
+            set
+            {
+                if (value != _city)
+                {
+                    _city = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Status
+        {
+            get => _status;
+            set
+            {
+                if (value != _status)
+                {
+                    _status = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                if (value != _type)
+                {
+                    _type = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime StartDate
+        {
+            get => _startDate;
+            set
+            {
+                if (value != _startDate)
+                {
+                    _startDate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public TourRequestOverview(User user)
+        {
+            InitializeComponent();
+            DataContext = this;
+            LoggedInUser = user;
+            _repository = new TourRequestRepository();
+            TourRequests = new ObservableCollection<TourRequest>(_repository.GetAllTourRequests());
+        }
+
+
+        private void LogOut(object sender, RoutedEventArgs e)
+        {
+            SignInForm signInForm = new SignInForm();
+            signInForm.Show();
+            Close();
+        }
+
+
+        private void ShowVouchers(object sender, RoutedEventArgs e)
+        {
+            Vouchers vouchers = new Vouchers(LoggedInUser);
+            vouchers.Show();
+            Close();
+        }
+
+        private void Ratings(object sender, RoutedEventArgs e)
+        {
+            ShowPastTours ratings = new ShowPastTours(LoggedInUser);
+            ratings.Show();
+            Close();
+        }
+
+        private void Active(object sender, RoutedEventArgs e)
+        {
+            ActiveTour activeTour = new ActiveTour(LoggedInUser);
+            activeTour.Show();
+            Close();
+        }
+
+        private void Requests(object sender, RoutedEventArgs e)
+        {
+            TourRequestOverview tourRequestOverview = new TourRequestOverview(LoggedInUser);
+            tourRequestOverview.Show();
+            Close();
+        }
+
+        private void Home(object sender, RoutedEventArgs e)
+        {
+            TourOverview tourOverview = new TourOverview(LoggedInUser);
+            tourOverview.Show();
+            Close();
+        }
+
+        private void CreateRequest(object sender, RoutedEventArgs e)
+        {
+            CreateTourRequest createTourRequest = new CreateTourRequest(LoggedInUser);
+            createTourRequest.Show();
+            Close();
+        }
+    }
+
+}
