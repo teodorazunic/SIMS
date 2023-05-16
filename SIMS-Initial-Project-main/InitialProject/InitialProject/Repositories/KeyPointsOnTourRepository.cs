127308aff69af1e67b3b0f134474425fd04b55ab
dunja037 <106025908+dunja037@users.noreply.github.com>
2023-05-16 17:07:34 +0200

    for hci

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Repositories/KeyPointsOnTourRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using InitialProject.Domain.Models;
+using InitialProject.Serializer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialProject.Repository;
+
+namespace InitialProject.Repositories
+{
+    public class KeyPointsOnTourRepository
+    {
+        private const string FilePath = "../../../Resources/Data/keypointsontour.csv";
+
+        private readonly Serializer<KeyPointsOnTour> _serializer;
+
+        private List<KeyPointsOnTour> _keyPointsOnTour;
+
+        public KeyPointsOnTourRepository()
+        {
+            _serializer = new Serializer<KeyPointsOnTour>();
+            //_tourRepository = new TourRepository();
+            _keyPointsOnTour = _serializer.FromCSV(FilePath);
+        }
+
+        public KeyPointsOnTour GetKeyPointById(int id)
+        {
+            _keyPointsOnTour = _serializer.FromCSV(FilePath);
+            return _keyPointsOnTour.Find(t => t.TourId == id);
+        }
+    }
+}
