00cb0854bba7b5e47498c13e81b925e37ebc393a
teodorazunic <teodorazunic@gmail.com>
2023-06-12 00:05:02 +0200

    stat

--- SIMS-Initial-Project-main/InitialProject/InitialProject/WPF/Views/Guide/TourStatistics.xaml.cs
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/WPF/Views/Guide/TourStatistics.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using DevExpress.Data.Browsing;
 using InitialProject.Domain.Models;
 using InitialProject.Repositories;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Definitions.Series;
+using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -35,6 +41,54 @@ namespace InitialProject.WPF.Views.Guide
 
         public TourReservations selectedTour;
 
+        private SeriesCollection _data;
+
+        private SeriesCollection data;
+
+      
+
+        private int AcceptedCount { get; set; }
+        private int DeniedCount { get; set; }
+
+        private int Age1 { get; set; }
+        private int Age2 { get; set; }
+        private int Age3 { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public SeriesCollection DataChart
+        {
+            get => _data;
+            set
+            {
+                if (_data != value)
+                {
+                    _data = value;
+                    OnPropertyChanged();
+                }
+
+            }
+        }
+
+        public SeriesCollection DataChart1
+        {
+            get => data;
+            set
+            {
+                if (data != value)
+                {
+                    data = value;
+                    OnPropertyChanged();
+                }
+
+            }
+        }
+
 
         public TourStatistics(User user)
         {
@@ -44,8 +98,57 @@ namespace InitialProject.WPF.Views.Guide
             _tourReservationRepositery = new TourReservationRepositery();
             List<int> vouchers = new List<int>();
             vouchers = reservationRepositery.GetVoucherStatistics(reservations);
-            txtUsed.Text = vouchers[0].ToString();
-            txtUnused.Text = vouchers[1].ToString();
+            //txtUsed.Text = vouchers[0].ToString();
+            //txtUnused.Text = vouchers[1].ToString();
+
+            
+            AcceptedCount = vouchers[0];
+            DeniedCount = vouchers[1];
+            
+            
+            DataChart = new SeriesCollection
+            {
+                new PieSeries
+            {
+                Title = "Accepted",
+                Values = new ChartValues<ObservableValue> {new ObservableValue(AcceptedCount)}
+            },
+                new PieSeries
+            {
+                Title = "Denied",
+                Values = new ChartValues<ObservableValue> { new ObservableValue(DeniedCount)}
+            }
+            };
+
+
+
+            Age1 = 5;
+            Age2 = 10;
+            Age3 = 4;
+
+            DataChart1 = new SeriesCollection
+            {
+                new PieSeries
+            {
+                Title = "18",
+                Values = new ChartValues<ObservableValue> {new ObservableValue(Age1)}
+            },
+                new PieSeries
+            {
+                Title = "18-55",
+                Values = new ChartValues<ObservableValue> { new ObservableValue(Age2)}
+            },
+                new PieSeries
+            {
+                Title = "55",
+                Values = new ChartValues<ObservableValue> { new ObservableValue(Age3)}
+            }
+
+            };
+
+
+
+
 
         }
 
@@ -66,6 +169,7 @@ namespace InitialProject.WPF.Views.Guide
             ComboBox.Items.Add("2023");
             ComboBox.Items.Add("2022");
             ComboBox.Items.Add("2021");
+            ComboBox.Items.Add("All years");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -77,6 +181,8 @@ namespace InitialProject.WPF.Views.Guide
             tourReservations.Add(tourReservation);
             MostVisitedTour.ItemsSource = tourReservations;
 
+            
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -84,9 +190,14 @@ namespace InitialProject.WPF.Views.Guide
             selectedTour = (TourReservations)MostVisitedTour.SelectedItem;
             int[] Info = new int[3];
             Info = _tourReservationRepositery.ShowStatistic(selectedTour.Tour.Id);
-            txt1.Text = Info[0].ToString();
-            txt2.Text = Info[1].ToString();
-            txt3.Text = Info[2].ToString();
+            //txt1.Text = Info[0].ToString();
+            //txt2.Text = Info[1].ToString();
+            //txt3.Text = Info[2].ToString();
+
+            Age1 = 5;
+            Age2 = 6;
+            Age3 = 2;
+           
 
         }
     }
